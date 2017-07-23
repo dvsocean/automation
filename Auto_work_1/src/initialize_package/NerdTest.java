380966e1c8520cb33a8dec6bf286f6826aa34202
dvsocean <dvsocean@icloud.com>
2017-07-23 15:02:32 -0700

    first

---
+++ Auto_work_1/src/initialize_package/NerdTest.java
@@ -0,0 +1,13 @@
+package initialize_package;
+
+public class NerdTest {
+	
+	public static String text;
+
+	public static void main(String[] args) {
+		text="HEY YOU";
+		System.out.println(text);
+
+	}
+
+}
